2ff2d623095a5f0c0fb1cf5d28ecaeb244b2f6e1
tianzhichao <tyronextian@gmail.com>
2024-06-12 09:52:23 +0800

    [FEATURE] test

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: tianzhichao
+File: test.py
+Time: 2024/6/12 9:51 AM
+"""
+import os
+
+
+def a():
+    # 测试文件 # 测试文件# 测试文件# 测试文件# 测试文件# 测试文件# 测试文件# 测试文件# 测试文件# 测试文件# 测试文件# 测试文件# 测试文件# 测试文件# 测试文件# 测试文件# 测试文件# 测试文件
+    pass
+
+if __name__ == '__main__':
+    a()
