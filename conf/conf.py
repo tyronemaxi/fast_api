6467e05e96b15eb29399d772076deea866441517
tyronemaxi <tyronextian@gmail.com>
2024-10-23 22:45:01 +0800

    [FEATURE] 流式响应例子

--- conf/conf.py
+++ conf/conf.py
@@ -11,5 +11,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-OPENAI_API_BASE = os.getenv("OPENAI_API_BASE")
+# [openai]
+api_base = os.getenv("api_base")
+api_key = os.getenv("api_key")
--- server.py
+++ server.py
@@ -5,27 +5,33 @@ Author: tianzhichao
 File: server.py
 Time: 2024/5/13 1:37 PM
 """
+from typing import AsyncGenerator
+
 from pydantic import BaseModel
 
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
-from app.client.llm_cli import call_llm
+from app.controller.chat.completions import chat_ctrl
 
 app = FastAPI()
 
 
-class StreamRequest(BaseModel):
-    """
-    Request body for streaming
-    """
-
-    message: str
+# 定义数据模型
+class Item(BaseModel):
+    query: str
+    stream: bool = True
 
 
 @app.post("/chat")
-def stream(body: StreamRequest):
-    return StreamingResponse(call_llm(body.message), media_type="text/event-stream")
+async def chat_endpoint(item: Item):
+    async def event_stream() -> AsyncGenerator[str, None]:
+        async for response in chat_ctrl.chat(
+            item.query, "1111", "2222", "33333", item.stream
+        ):
+            yield f"data: {response}\n\n"
+
+    return StreamingResponse(event_stream(), media_type="text/event-stream")
 
 
 if __name__ == "__main__":
